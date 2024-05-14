8c4b005a916821ce36550a835efd3d46dae827fb
Nayborodin <158287255+Nayborodin@users.noreply.github.com>
2024-05-14 15:46:45 +0300

    доповнення до сайту "market"

--- market.js
+++ market.js
@@ -1,23 +1,25 @@
-
 //Перевірка підключеного файлу 
-console.log('Перевірка підключеного файлу скриптів market.js')
+//onsole.log('Перевірка підключеного файлу скриптів market.js')
+
+//визначення масиву товарів
+let itemsArray = [
+    'Газонокосарка 43',
+    'Електричний тример 110',
+    'Електрична газонокосарка 32',
+    'Фкумуляторний оприскувач 12N',
+]
 
 //отримання елементу з ідентифікатором items
 let itemsDiv=document.getElementById("items");
 
 //перевірка існування знайденого блоку
 if (itemsDiv) {
-    //вивід знайденого елементу
-    console.log(itemsDiv)
-    //вивід значення поля знайденого елементу
-    console.log('Поле classList: ', itemsDiv.classList)
-    console.log('Поле id: ', itemsDiv.id)
-    console.log('Поле clientWidth: ', itemsDiv.clientWidth)
-    console.log('Поле innerHTML: ', itemsDiv.innerHTML)
+    //вивід елементу масиву
+    itemsArray.forEach((item)=>{
+        //console.log(item) 
+        itemsDiv.innerHTML +=`<div class = "item">${item}</div>`
+    })
 
-    for (i=0; i < 100; i++) {
-        itemsDiv.innerHTML += '<div class="item"></div>'
-    }
 } else {
     //вивід повідомлення про не знайдений блок
     console.log('Блок товарів не знайдено')
